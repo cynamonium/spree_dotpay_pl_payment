2e332a401dd9fc1b00556277551bc58c9da5c706
piotr <piotr@piotr-945GCM-S2L.(none)>
2013-05-13 12:55:45 +0200

    change

--- app/controllers/gateway/dotpay_pl_controller.rb
+++ app/controllers/gateway/dotpay_pl_controller.rb
@@ -53,7 +53,7 @@ class Gateway::DotpayPlController < Spree::BaseController
   # validating dotpay message
   def dotpay_pl_validate(gateway, params, remote_ip)
 
-    param_args = "oasOjteabUtgusdo:" +
+    param_args =  (gateway.preferred_urlc_pin.nil? ? "" : gateway.preferred_urlc_pin) + ":" +
       (params[:id].nil? ? "" : params[:id]) + ":" +
       (params[:control].nil? ? "" : params[:control]) + ":" +
       (params[:t_id].nil? ? "" : params[:t_id]) + ":" +
@@ -81,7 +81,7 @@ class Gateway::DotpayPlController < Spree::BaseController
     end
 
     order.finalize!
-
+    order.payment_state = 'paid'
     order.next
     order.next
     order.save
